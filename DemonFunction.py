cd0072200b62763b989b765a57af26583b368af4
lebaohongnhat <71966641+lebaohongnhat@users.noreply.github.com>
2020-09-27 17:06:58 +0700

    The first upload

---
+++ DemonFunction.py
@@ -0,0 +1,9 @@
+#nhan_doi=lambda a:a*2
+#print(nhan_doi(4))
+
+danh_sach=[1,2,3,4,5,6,7,8,9,10]
+"""danh_sach_chan=list(filter(lambda a:a%2==0,danh_sach))
+print(danh_sach_chan)"""
+
+danh_sach_nhan_doi=list(map(lambda a:a*2,danh_sach))
+print(danh_sach_nhan_doi)
